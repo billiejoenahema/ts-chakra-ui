26e2dec6663295de8e67f3139ba9c2d6a4d56bf7
billiejoenahema <billiejoenahema@gmail.com>
2021-09-09 20:32:38 +0900

    apply: chakra-ui style to login page

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,10 +1,18 @@
 import { VFC, memo } from 'react'
+import { Box, Button, Divider, Flex, Heading, Input, Stack } from '@chakra-ui/react'
 
 const Login: VFC = memo(() => {
   return (
-    <div>
-      ログインページです
-    </div>
+    <Flex align="center" justify="center" height="100vh" >
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md" >
+        <Heading as="h1" size="lg" textAlign="center" >ユーザー管理アプリ</Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10} >
+          <Input placeholder="ユーザーID" />
+          <Button bg="teal" color="white" _hover={{ opacity: 0.8 }} >ログイン</Button>
+        </Stack>
+      </Box>
+    </Flex>
   )
 })
 
