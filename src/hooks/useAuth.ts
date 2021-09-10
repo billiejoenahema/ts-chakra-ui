011ecec2d5b247e92ab9633c23f69a54b9cd2909
billiejoenahema <billiejoenahema@gmail.com>
2021-09-10 18:51:57 +0900

    apply: chakra-ui Toast

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,11 @@ import axios from 'axios'
 import React from 'react'
 import { useHistory } from 'react-router'
 import { User } from '../types/api/user'
+import { useMessage } from './useMessage'
 
 export const useAuth = () => {
   const history = useHistory()
+  const showMessage = useMessage()
   const [loading, setLoading] = React.useState(false)
 
   const login = React.useCallback((id: string) => {
@@ -13,13 +15,17 @@ export const useAuth = () => {
       .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((res) => {
         if (res.data) {
+          showMessage({
+            title: 'ログインしました',
+            status: 'success'
+          })
           history.push('/home')
         } else {
-          alert('ユーザーが見つかりません')
+          showMessage({ title: 'ユーザーが見つかりません', status: 'error' })
         }
       })
-      .catch(() => alert('ログインできませんでした'))
+      .catch(() => showMessage({ title: 'ログインできませんでした', status: 'error' }))
       .finally(() => setLoading(false))
-  }, [history])
+  }, [history, showMessage])
   return { login, loading }
 }
